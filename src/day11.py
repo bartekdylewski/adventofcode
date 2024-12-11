412db48c3941302448988964421453e82ec424a6
bartekdylewski <dylewskipoczta@gmail.com>
2024-12-11 06:56:38 +0100

    day11 *, needs optimization

---
+++ src/day11.py
@@ -0,0 +1,44 @@
+test_ = "125 17".split()
+input_ = open("input/input11.txt").read().strip().split()
+
+def int_list(list):
+    return [int(i) for i in list]
+
+def blink(list):
+    new_list = []
+    for stone in list:
+        # if s == 0, replace by s == 1
+        # if even digits, split stone in halves
+        # else s *= 2024
+        digits = len(str(stone))
+        # print(digits)
+        
+        if int(stone) == 0:
+            new_list.append(1)
+        elif digits % 2 == 0:
+            mid = digits // 2
+            num1 = str(stone)[:mid]
+            num2 = str(stone)[mid:]
+            # print(f" {num1=} {num2=}")
+            new_list.append(int(num1))
+            new_list.append(int(num2))
+        else:
+            new_list.append(int(stone) * 2024)
+            
+    return new_list
+
+def main():
+    # test = int_list(test_)
+    # input = int_list(input_)
+    # print("TEST:  ",blink(test_))
+    list = int_list(input_)
+    print("0 : ",list)
+    current = list
+    for i in range(1,76):
+        current = blink(current)
+        # print(i,": ",current)
+        print(i, len(current))
+    print("len(blink25)= ",len(current))
+    # 50min for part1
+    
+main()
