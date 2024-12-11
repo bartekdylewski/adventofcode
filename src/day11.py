228d404606f2cf2ea984ebd3797aaa57c608fba3
bartekdylewski <dylewskipoczta@gmail.com>
2024-12-11 14:14:58 +0100

    day11 ** own code

--- src/day11.py
+++ src/day11.py
@@ -1,41 +1,66 @@
-test_ = "125 17".split()
-input_ = open("input/input11.txt").read().strip().split()
+import functools
 
-def int_list(list):
-    return [int(i) for i in list]
+test_ = "125 17"
+input_ = open("input/input11.txt", "r").read()
 
-def blink(list):
-    new_list = []
-    for stone in list:
-        # if s == 0, replace by s == 1
-        # if even digits, split stone in halves
-        # else s *= 2024
-        digits = len(str(stone))
-        # print(digits)
+stones = list(map(int, input_.split()))
+
+@functools.lru_cache(maxsize=None)
+def single_blink_stone(value):
+
+    text = str(value)
+    digits = len(text)
+    
+    if value == 0:
+        return (1, None)
+    elif digits % 2 == 0:
+        mid = digits // 2
+        num1 = int(text[:mid])
+        num2 = int(text[mid:])
+        return (num1, num2)
+    else:
+        return (value * 2024, None)
+
+@functools.lru_cache(maxsize=None)
+def count_stone_blinks(stone, depth):
+    
+    left_stone, right_stone = single_blink_stone(stone)
+    
+    # last iteration
+    if depth == 1:
         
-        if int(stone) == 0:
-            new_list.append(1)
-        elif digits % 2 == 0:
-            mid = digits // 2
-            num1 = str(stone)[:mid]
-            num2 = str(stone)[mid:]
-            # print(f" {num1=} {num2=}")
-            new_list.append(int(num1))
-            new_list.append(int(num2))
+        # count if one or two stones
+        if right_stone is None:
+            return 1
         else:
-            new_list.append(int(stone) * 2024)
-            
-    return new_list
+            return 2
+    
+    else:
+        # recurse to next level and add stones if there are two
+        output = count_stone_blinks(left_stone, depth - 1)
+        if right_stone is not None:
+            output += count_stone_blinks(right_stone, depth - 1)
+        
+        return output
+    
+
+def run(count):
+    output = 0
+    
+    # look at each stone
+    for stone in stones:
+        
+        # how many stones does one turn into?
+        output += count_stone_blinks(stone, count)
+        
+    return output
 
 def main():
-    list = int_list(test_)
-    print("0 : ",list)
-    current = list
-    for i in range(1,76):
-        current = blink(current)
-        # print(i,": ",current)
-        print(i, len(current))
-    print("len(blink25)= ",len(current))
-    # 50min for part1
     
+    out25 = run(25)
+    out75 = run(75)
+    
+    print(f"Part 1: {out25}")
+    print(f"Part 2: {out75}")
+   
 main()
